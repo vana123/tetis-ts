e53f409952a19027e975551f8885177f7f26955c
Vaana <35535603+vana123@users.noreply.github.com>
2022-05-20 15:16:28 +0300

    first commit

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+
+// Import Tetris component
+import Tetris from './components/tetris'
+
+// Import styles
+import './styles/styles.css'
+
+ReactDOM.render(<Tetris boardWidth="14" boardHeight="20" />, document.getElementById('root'))
