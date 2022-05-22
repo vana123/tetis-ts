990411febceaa168ca07aae0275a55604e8f3634
Vaana <35535603+vana123@users.noreply.github.com>
2022-05-22 20:05:54 +0300

    Updates

---
+++ precache-manifest.df36e8f192a8683cbb0a8aaf9ff4a59b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39726d850c374871ec315dc3f99c3542",
+    "url": "/tetis-ts/index.html"
+  },
+  {
+    "revision": "cc1cd7cfa0999ce50824",
+    "url": "/tetis-ts/static/css/main.6a0f2e60.chunk.css"
+  },
+  {
+    "revision": "529967f4324721c1ab52",
+    "url": "/tetis-ts/static/js/2.a5ebfcba.chunk.js"
+  },
+  {
+    "revision": "cc1cd7cfa0999ce50824",
+    "url": "/tetis-ts/static/js/main.0877d808.chunk.js"
+  },
+  {
+    "revision": "6122dcc9c57aff64b538",
+    "url": "/tetis-ts/static/js/runtime~main.bdaf87c9.js"
+  }
+]);
